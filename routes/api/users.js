4dda688385ab94a105f843ecc1e6cb46c5addb56
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-05-29 14:42:04 +0200

    api routes, register endpoint, passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,4 +2,112 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const key = require("../../config/key");
+const keys = require("../../config/key");
+
+//load input validation
+const validationRegisterInput = require("../../validation/register");
+const validationLoginInput = require("../../validation/login");
+
+//load User model
+const User = require("../../models/Users");
+
+router.post('/register', (req, res) => {
+  //form validation
+
+  const {
+    errors,
+    isValid
+  } = validationRegisterInput(req.body);
+
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: 'Email już istnieje'
+      });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      // hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err))
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', (req, res) => {
+  //form validation
+  const {
+    errors,
+    isValid
+  } = validationLoginInput(req.body);
+
+  //check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find user by email
+
+  User.findById({
+    email
+  }).then(user => {
+    //check if user exists
+    if (!user) {
+      return res.status(404).json({
+        emailnotfound: "Nie znaleziono emaila"
+      });
+    }
+
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //user matched
+        //create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+
+        //sign token
+        jwt.sign(payload, keys.secretOrKey, {
+            expiresIn: 31556926 //one year in seconds
+          },
+          (err, token) => {
+            res.json({
+              succes: true,
+              token: 'Bearer ' + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({
+          passwordincorrect: "Hasło jest nieprawidłowe"
+        });
+      }
+    });
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const app = express()
+const users = require('./routes/api/users');
+
+const app = express();
 
 //bodyparser for middleware
 app.use(bodyParser.urlencoded({
   extended: true
 }))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/key').mongoURI;
@@ -20,6 +23,15 @@ mongoose.connect(db, {
   .then(console.log('MongoDB succesfully connected'))
   .catch(error => console.log(error));
 
+// passport middleware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport);
+
+//routes
+app.use('/api/users', users);
+
 const port = process.env.PORT || 5000 //for Heroku port
 
-app.listen(port, () => console.log(`server is running on port ${port}, running all the time :)`))
+app.listen(port, () => console.log(`server is running on port ${port}, is running all the time :)`))
