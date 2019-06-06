d204c1dbb93ce0ec2de13849d7007f90e4a2a768
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-06-06 16:36:10 +0200

    change in Login and register file

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../action/authActions";
+import classnames from "classnames";
 
 class Login extends Component {
   state = {
@@ -8,14 +12,35 @@ class Login extends Component {
     errors: {}
   };
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard"); // push user to dashboard when they login
+    }
+
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
   onChange = e => {
     this.setState({
       [e.target.id]: e.target.value
     });
   };
+
   onSubmit = e => {
     e.preventDefault();
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
   };
+
   render() {
     const { errors, email, password } = this.state;
     return (
@@ -42,8 +67,15 @@ class Login extends Component {
                   value={email}
                   id="email"
                   error={errors.email}
+                  className={classnames("", {
+                    invalid: errors.email || errors.emailnotfound
+                  })}
                 />
                 <label htmlFor="email">Email</label>
+                <span className="red-text">
+                  {errors.email}
+                  {errors.emailnotfound}
+                </span>
               </div>
               <div className="input-field col s12">
                 <input
@@ -52,8 +84,15 @@ class Login extends Component {
                   value={password}
                   id="password"
                   error={errors.password}
+                  className={classnames("", {
+                    invalid: errors.password || errors.passwordincorrect
+                  })}
                 />
                 <label htmlFor="password">Hasło</label>
+                <span className="red-text">
+                  {errors.password}
+                  {errors.passwordincorrect}
+                </span>
               </div>
               <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                 <button
@@ -76,4 +115,19 @@ class Login extends Component {
     );
   }
 }
-export default Login;
+
+Login.propTypes = {
+  looginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);
--- server.js
+++ server.js
@@ -1,37 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users = require('./routes/api/users');
+const users = require("./routes/api/users");
 
 const app = express();
 
 //bodyparser for middleware
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
 
 // DB config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //connect to mongoDB
-mongoose.connect(db, {
+mongoose
+  .connect(db, {
     useNewUrlParser: true
   })
-  .then(console.log('MongoDB succesfully connected'))
+  .then(console.log("MongoDB succesfully connected"))
   .catch(error => console.log(error));
 
 // passport middleware
 app.use(passport.initialize());
 
 //passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //routes
-app.use('/api/users', users);
+app.use("/api/users", users);
 
-const port = process.env.PORT || 5000 //for Heroku port
+const port = process.env.PORT || 5000; //for Heroku port
 
-app.listen(port, () => console.log(`server is running on port ${port}, is running all the time :)`))
+app.listen(port, () =>
+  console.log(`server is running on port ${port}, is running all the time :)`)
+);
